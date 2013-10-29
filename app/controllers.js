eccb9312013be90d74082e017194d69fa9d71a66
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-10-29 17:33:08 -0200

    Alteração do comportamento da autenticação.

--- app/controllers.js
+++ app/controllers.js
@@ -1,12 +1,27 @@
 'use strict';
 
 angular.module('siges.controllers', []).
-    controller('EntrarCtrl', ['$scope', function ($scope) {
-        $scope.usuario = {
-            email: 'alanclesio@gmail.com',
-            senha: '123456'
-        };
+    controller('EntrarCtrl', ['$scope', 'Autenticacao', '$rootScope', '$location', '$timeout', function ($scope, Autenticacao, $rootScope, $location, $timeout) {
         $scope.login = function () {
-            console.log('Entrou')
+            jQuery('.modal').modal();
+            Autenticacao.auth.login('password', {
+                email: $scope.usuario.email,
+                password: $scope.usuario.senha,
+                rememberMe: $scope.usuario.lembrar
+            });
         };
+        $scope.logout = function () {
+            Autenticacao.auth.logout();
+            $scope.usuario = null;
+        };
+        $rootScope.$on("loggedin", function(event, user) {
+            $scope.usuario = user;
+            $timeout(function() {
+                $location.path('/');
+            });
+            jQuery('.modal').modal('hide');
+        })
+        $rootScope.$on("loggedout", function(event) {
+            $scope.usuario = null;
+        })
     }]);
--- app/services.js
+++ app/services.js
@@ -1,4 +1,47 @@
 'use strict';
 
 angular.module('siges.services', []).
-    value('version', '0.1');
+    value('version', '0.1').
+    service('Autenticacao', ['$rootScope', function ($rootScope) {
+        this.auth = new FirebaseSimpleLogin(new Firebase("https://siges.firebaseio.com"), function (error, user) {
+            if (user) {
+                $rootScope.$emit("loggedin", user);
+            }
+            else if (error) {
+                jQuery('.modal').modal('hide');
+                switch (error.code) {
+                    case 'AUTHENTICATION_DISABLED':
+                        bootbox.alert('<strong>Atenção</strong>, Autenticação desabilitada.')
+                        break;
+                    case 'EMAIL_TAKEN':
+                        bootbox.alert('<strong>Atenção</strong>, este e-mail já está em uso.')
+                        break;
+                    case 'INVALID_EMAIL':
+                        bootbox.alert('<strong>Atenção</strong>, este e-mail está incorreto.')
+                        break;
+                    case 'INVALID_FIREBASE':
+                        bootbox.alert('<strong>Atenção</strong>, banco inválido.')
+                        break;
+                    case 'INVALID_ORIGIN':
+                        bootbox.alert('<strong>Atenção</strong>, não é possível se logar através deste endereço.')
+                        break;
+                    case 'INVALID_PASSWORD':
+                        bootbox.alert('<strong>Atenção</strong>, a senha digitada é inválida.')
+                        break;
+                    case 'INVALID_USER':
+                        bootbox.alert('<strong>Atenção</strong>, usuário não existe.')
+                        break;
+                    case 'UNKNOWN_ERROR':
+                        bootbox.alert('<strong>Atenção</strong>, erro desconhecido, entre em contato com contato@tecla.me')
+                        break;
+                    case 'USER_DENIED':
+                        bootbox.alert('<strong>Atenção</strong>, você não tem permissão para acessar esta área.')
+                        break;
+                    default:
+                }
+            }
+            else {
+                $rootScope.$emit("loggedout");
+            }
+        });
+    }]);
