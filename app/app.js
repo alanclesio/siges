0b88cf074108b390fa320308f516eb1b99bad718
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-10-31 13:56:30 -0200

    Correção na validação do serviço de autenticação.

--- app/app.js
+++ app/app.js
@@ -19,3 +19,51 @@ angular.module('siges', [
             when('/404', {templateUrl: 'partials/404.html'}).
             otherwise({redirectTo: '/404'});
     }]);
+
+// configuração dos vendors
+
+bootbox.setDefaults({
+    /**
+     * @optional String
+     * @default: en
+     * which locale settings to use to translate the three
+     * standard button labels: OK, CONFIRM, CANCEL
+     */
+    locale: "br",
+
+    /**
+     * @optional Boolean
+     * @default: true
+     * whether the dialog should be shown immediately
+     */
+    show: true,
+
+    /**
+     * @optional Boolean
+     * @default: true
+     * whether the dialog should be have a backdrop or not
+     */
+    backdrop: true,
+
+    /**
+     * @optional Boolean
+     * @default: true
+     * show a close button
+     */
+    closeButton: true,
+
+    /**
+     * @optional Boolean
+     * @default: true
+     * animate the dialog in and out (not supported in < IE 10)
+     */
+    animate: false,
+
+    /**
+     * @optional String
+     * @default: null
+     * an additional class to apply to the dialog wrapper
+     */
+    className: "my-modal"
+
+});
--- app/services.js
+++ app/services.js
@@ -2,7 +2,7 @@
 
 angular.module('siges.services', []).
     value('version', '0.1').
-    service('Autenticacao', ['$rootScope', function ($rootScope) {
+    service('Autenticacao', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
         this.auth = new FirebaseSimpleLogin(new Firebase("https://siges.firebaseio.com"), function (error, user) {
             if (user) {
                 $rootScope.$emit("loggedin", user);
@@ -11,31 +11,43 @@ angular.module('siges.services', []).
                 jQuery('.modal').modal('hide');
                 switch (error.code) {
                     case 'AUTHENTICATION_DISABLED':
-                        bootbox.alert('<strong>Atenção</strong>, Autenticação desabilitada.')
+                        bootbox.alert('<strong>Atenção</strong>, Autenticação desabilitada.');
                         break;
                     case 'EMAIL_TAKEN':
-                        bootbox.alert('<strong>Atenção</strong>, este e-mail já está em uso.')
+                        bootbox.alert('<strong>Atenção</strong>, este e-mail já está em uso.');
                         break;
                     case 'INVALID_EMAIL':
-                        bootbox.alert('<strong>Atenção</strong>, este e-mail está incorreto.')
+                        bootbox.alert('<strong>Atenção</strong>, este e-mail está incorreto.', function(){
+                            $timeout(function(){
+                                jQuery('[name=email]').val('').focus();
+                            });
+                        });
                         break;
                     case 'INVALID_FIREBASE':
-                        bootbox.alert('<strong>Atenção</strong>, banco inválido.')
+                        bootbox.alert('<strong>Atenção</strong>, banco inválido.');
                         break;
                     case 'INVALID_ORIGIN':
-                        bootbox.alert('<strong>Atenção</strong>, não é possível se logar através deste endereço.')
+                        bootbox.alert('<strong>Atenção</strong>, não é possível se logar através deste endereço.');
                         break;
                     case 'INVALID_PASSWORD':
-                        bootbox.alert('<strong>Atenção</strong>, a senha digitada é inválida.')
+                        bootbox.alert('<strong>Atenção</strong>, a senha digitada é inválida.', function() {
+                            $timeout(function(){
+                                jQuery('[name=password]').val('').focus();
+                            });
+                        });
                         break;
                     case 'INVALID_USER':
-                        bootbox.alert('<strong>Atenção</strong>, usuário não existe.')
+                        bootbox.alert('<strong>Atenção</strong>, usuário não existe.', function(){
+                            $timeout(function(){
+                                jQuery('[name=email]').val('').focus();
+                            });
+                        });
                         break;
                     case 'UNKNOWN_ERROR':
-                        bootbox.alert('<strong>Atenção</strong>, erro desconhecido, entre em contato com contato@tecla.me')
+                        bootbox.alert('<strong>Atenção</strong>, erro desconhecido, entre em contato com contato@tecla.me');
                         break;
                     case 'USER_DENIED':
-                        bootbox.alert('<strong>Atenção</strong>, você não tem permissão para acessar esta área.')
+                        bootbox.alert('<strong>Atenção</strong>, você não tem permissão para acessar esta área.');
                         break;
                     default:
                 }
