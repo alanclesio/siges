84aa9f0e649a7d95b708bb881552e7bc7aba8b97
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-11-01 15:46:51 -0200

    Correção na validação do serviço de autenticação.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('siges', [
+        'md5',
         'firebase',
         'siges.factories',
         'siges.filters',
@@ -12,58 +13,14 @@ angular.module('siges', [
         $routeProvider.
             when('/', {templateUrl: 'partials/inicio.html'}).
             when('/usuarios/criar', {templateUrl: 'partials/usuario-detalhes.html', controller: 'UsuariosCriarCtrl'}).
-            when('/primeiro-acesso', {templateUrl: 'partials/primeiro-acesso.html', controller: 'PrimeiroAcessoCtrl'}).
             when('/usuarios/editar/:id', {templateUrl: 'partials/usuario-detalhes.html', controller: 'UsuariosEditarCtrl'}).
             when('/usuarios', {templateUrl: 'partials/usuarios-listar.html', controller: 'UsuariosListarCtrl'}).
+            when('/primeiro-acesso', {templateUrl: 'partials/primeiro-acesso.html', controller: 'PrimeiroAcessoCtrl'}).
+            when('/perfil', {templateUrl: 'partials/perfil.html', controller: 'PerfilCtrl'}).
             when('/entrar', {templateUrl: 'partials/entrar.html', controller: 'EntrarCtrl'}).
             when('/404', {templateUrl: 'partials/404.html'}).
             otherwise({redirectTo: '/404'});
     }]);
 
 // configuração dos vendors
-
-bootbox.setDefaults({
-    /**
-     * @optional String
-     * @default: en
-     * which locale settings to use to translate the three
-     * standard button labels: OK, CONFIRM, CANCEL
-     */
-    locale: "br",
-
-    /**
-     * @optional Boolean
-     * @default: true
-     * whether the dialog should be shown immediately
-     */
-    show: true,
-
-    /**
-     * @optional Boolean
-     * @default: true
-     * whether the dialog should be have a backdrop or not
-     */
-    backdrop: true,
-
-    /**
-     * @optional Boolean
-     * @default: true
-     * show a close button
-     */
-    closeButton: true,
-
-    /**
-     * @optional Boolean
-     * @default: true
-     * animate the dialog in and out (not supported in < IE 10)
-     */
-    animate: false,
-
-    /**
-     * @optional String
-     * @default: null
-     * an additional class to apply to the dialog wrapper
-     */
-    className: "my-modal"
-
-});
+bootbox.setDefaults({locale: "br", animate: false});
--- app/controllers.js
+++ app/controllers.js
@@ -2,23 +2,40 @@
 
 angular.module('siges.controllers', []).
     // controle responsável por manter usuários
-    controller('UsuariosCriarCtrl',[
+    controller('UsuariosCriarCtrl', [
         '$scope',
         '$location',
         '$timeout',
         'Usuarios',
-        function($scope, $location, $timeout, Usuarios){
-        $scope.salvar = function() {
-            Usuarios.add($scope.usuario, function() {
-                bootbox.alert('O usuário <strong>' + $scope.usuario.nome + '</strong> foi cadastrado com sucesso!', function() {
-                    $timeout(function() {
-                        $location.path('/usuarios/criar');
-                        $scope.usuario = null;
-                    });
+        'md5',
+        function ($scope, $location, $timeout, Usuarios, md5) {
+            $scope.salvar = function () {
+                var usuarioCadastrado;
+                angular.forEach(Usuarios, function (usuario, key) {
+                    if ($scope.usuario.email == usuario.email) {
+                        usuarioCadastrado = true;
+                    }
                 });
-            });
-        }
-    }]).
+                if (!usuarioCadastrado) {
+                    $scope.usuario.md5_hash = md5.createHash($scope.usuario.email.toLowerCase());
+                    Usuarios.add($scope.usuario, function () {
+                        bootbox.alert('O usuário <strong>' + $scope.usuario.nome + '</strong> foi cadastrado com sucesso!', function () {
+                            $timeout(function () {
+                                $location.path('/usuarios/criar');
+                                $scope.usuario = null;
+                            });
+                        });
+                    });
+                } else {
+                    bootbox.alert('Atenção, o usuário <strong>' + $scope.usuario.nome + '</strong> não pôde ser cadastrado pois já há um cadastro com este e-mail!', function () {
+                        $timeout(function () {
+                            $location.path('/usuarios/criar');
+                            $scope.usuario = null;
+                        });
+                    });
+                }
+            };
+        }]).
     controller('UsuariosEditarCtrl').
     controller('UsuariosListarCtrl').
     controller('PrimeiroAcessoCtrl', [
@@ -27,67 +44,73 @@ angular.module('siges.controllers', []).
         '$location',
         'Autenticacao',
         'Usuarios',
-        function($scope, $timeout, $location, Autenticacao, Usuarios){
-        $scope.usuario = {
-            email:'zelda@gmail.com'
-        }
-        $scope.entrar = function() {
-            var preCadastro = 0;
-            angular.forEach(Usuarios, function(usuario, key){
-                if($scope.usuario.email == usuario.email){
-                    preCadastro = 1;
-                    $scope.usuario.nome = usuario.nome;
-                    jQuery('.modal').modal();
-                    Autenticacao.auth.createUser($scope.usuario.email, $scope.usuario.password, function(error, user) {
-                        if (!error) {
-                            jQuery('.modal').modal('hide');
-                            console.log('User Id: ' + user.id + ', Email: ' + user.email);
-                            bootbox.alert('Parabéns <strong>' + $scope.usuario.nome + '</strong>, agora efetue seu login!', function() {
-                                $timeout(function() {
-                                    $location.path('/entrar');
+        function ($scope, $timeout, $location, Autenticacao, Usuarios) {
+            $scope.entrar = function () {
+                var preCadastro = 0;
+                angular.forEach(Usuarios, function (usuario, key) {
+                    if ($scope.usuario.email == usuario.email) {
+                        preCadastro = 1;
+                        jQuery('.modal').modal();
+                        Autenticacao.auth.createUser($scope.usuario.email, $scope.usuario.password, function (error, user) {
+                            if (!error) {
+                                jQuery('.modal').modal('hide');
+                                $scope.usuario.nome = usuario.nome;
+                                bootbox.alert('Parabéns <strong>' + $scope.usuario.nome + '</strong>, agora efetue seu login!', function () {
+                                    $timeout(function () {
+                                        $location.path('/entrar');
+                                    });
                                 });
-                            });
-                        } else {
-                            jQuery('.modal').modal('hide');
-                            switch (error.code) {
-                                case 'AUTHENTICATION_DISABLED':
-                                    bootbox.alert('<strong>Atenção</strong>, Autenticação desabilitada.')
-                                    break;
-                                case 'EMAIL_TAKEN':
-                                    bootbox.alert('<strong>Atenção</strong>, este e-mail já está em uso.')
-                                    break;
-                                case 'INVALID_EMAIL':
-                                    bootbox.alert('<strong>Atenção</strong>, este e-mail está incorreto.')
-                                    break;
-                                case 'INVALID_FIREBASE':
-                                    bootbox.alert('<strong>Atenção</strong>, banco inválido.')
-                                    break;
-                                case 'INVALID_ORIGIN':
-                                    bootbox.alert('<strong>Atenção</strong>, não é possível se logar através deste endereço.')
-                                    break;
-                                case 'INVALID_PASSWORD':
-                                    bootbox.alert('<strong>Atenção</strong>, a senha digitada é inválida.')
-                                    break;
-                                case 'INVALID_USER':
-                                    bootbox.alert('<strong>Atenção</strong>, usuário não existe.')
-                                    break;
-                                case 'UNKNOWN_ERROR':
-                                    bootbox.alert('<strong>Atenção</strong>, erro desconhecido, entre em contato com contato@tecla.me')
-                                    break;
-                                case 'USER_DENIED':
-                                    bootbox.alert('<strong>Atenção</strong>, você não tem permissão para acessar esta área.')
-                                    break;
-                                default:
+                            } else {
+                                jQuery('.modal').modal('hide');
+                                switch (error.code) {
+                                    case 'AUTHENTICATION_DISABLED':
+                                        bootbox.alert('<strong>Atenção</strong>, Autenticação desabilitada.');
+                                        break;
+                                    case 'EMAIL_TAKEN':
+                                        bootbox.alert('<strong>Atenção</strong>, este e-mail já está em uso.', function () {
+                                            $timeout(function () {
+                                                jQuery('[name=email]').val('').focus();
+                                            });
+                                        });
+                                        break;
+                                    case 'INVALID_EMAIL':
+                                        bootbox.alert('<strong>Atenção</strong>, este e-mail está incorreto.');
+                                        break;
+                                    case 'INVALID_FIREBASE':
+                                        bootbox.alert('<strong>Atenção</strong>, banco inválido.');
+                                        break;
+                                    case 'INVALID_ORIGIN':
+                                        bootbox.alert('<strong>Atenção</strong>, não é possível se logar através deste endereço.');
+                                        break;
+                                    case 'INVALID_PASSWORD':
+                                        bootbox.alert('<strong>Atenção</strong>, a senha digitada é inválida.');
+                                        break;
+                                    case 'INVALID_USER':
+                                        bootbox.alert('<strong>Atenção</strong>, usuário não existe.');
+                                        break;
+                                    case 'UNKNOWN_ERROR':
+                                        bootbox.alert('<strong>Atenção</strong>, erro desconhecido, entre em contato com contato@tecla.me');
+                                        break;
+                                    case 'USER_DENIED':
+                                        bootbox.alert('<strong>Atenção</strong>, você não tem permissão para acessar esta área.');
+                                        break;
+                                    default:
+                                }
                             }
-                        }
+                        });
+                        return;
+                    }
+                });
+                if (!preCadastro) {
+                    bootbox.alert('Ainda não foi realizado o seu pré-cadastro, entre em contato com o administrador. Você será redirecionado para a página inicial!', function () {
+                        $timeout(function () {
+                            $location.path('/');
+                        });
                     });
-                    return;
                 }
-            });
-            if (!preCadastro){
-                bootbox.alert('Ainda não foi realizado o seu pré-cadastro');
             }
-        }
+        }]).
+    controller('PerfilCtrl', ['$scope', function ($scope) {
     }]).
     // controle responsável pela autenticação de usuários
     controller('EntrarCtrl', [
@@ -97,34 +120,44 @@ angular.module('siges.controllers', []).
         '$rootScope',
         '$location',
         '$timeout', function ($scope, Usuarios, Autenticacao, $rootScope, $location, $timeout) {
-        if($scope.usuarioLogado){
-            if($location.$$path == '/entrar'){
-                $location.path('/');
-            }
-        }
-        $scope.login = function () {
-            jQuery('.modal').modal();
-            Autenticacao.auth.login('password', {
-                email: $scope.usuario.email,
-                password: $scope.usuario.senha,
-                rememberMe: $scope.usuario.lembrar
-            });
-        };
-        $scope.logout = function () {
-            Autenticacao.auth.logout();
-            $scope.usuarioLogado = null;
-        };
-        $rootScope.$on("loggedin", function(event, user) {
-            $scope.usuarioLogado = user;
-            console.info($location.$$path);
-            $timeout(function(){
-                if($location.$$path == '/entrar'){
+            if ($scope.usuarioLogado) {
+                if ($location.$$path == '/entrar') {
                     $location.path('/');
                 }
-                jQuery('.modal').modal('hide');
+            }
+            $scope.login = function () {
+                jQuery('.modal').modal();
+                Autenticacao.auth.login('password', {
+                    email: $scope.usuario.email,
+                    password: $scope.usuario.senha,
+                    rememberMe: $scope.usuario.lembrar
+                });
+            };
+            $scope.logout = function () {
+                Autenticacao.auth.logout();
+                $scope.usuarioLogado = null;
+            };
+            $rootScope.$on("loggedin", function (event, user) {
+                var stop;
+                stop = $timeout(function () {
+                    if (Usuarios) { // verificar o logon de usuários
+                        if (!$scope.usuarioLogado) {
+                            angular.forEach(Usuarios, function (usuario, key) {
+                                if (user.md5_hash == usuario.md5_hash) {
+                                    $scope.usuarioLogado = usuario;
+                                }
+                            });
+                            if ($location.$$path == '/entrar') {
+                                $location.path('/');
+                            }
+                        }
+                    } else {
+                        $timeout.cancel(stop);
+                    }
+                    jQuery('.modal').modal('hide');
+                }, 1000);
             });
-        });
-        $rootScope.$on("loggedout", function(event) {
-            $scope.usuarioLogado = null;
-        });
-    }]);
+            $rootScope.$on("loggedout", function (event) {
+                $scope.usuarioLogado = null;
+            });
+        }]);
