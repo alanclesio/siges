ac1836ccb401ac8516f5e1e5f418c2f09c0ec3b3
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-11-10 15:37:35 -0200

    correcao layout e autenticação

--- app/filters.js
+++ app/filters.js
@@ -1,8 +1,25 @@
 'use strict';
 
 angular.module('siges.filters', []).
-    filter('interpolate', ['version', function (version) {
+    filter('quebralinha', function () {
         return function (text) {
-            return String(text).replace(/\%VERSION\%/mg, version);
+            return (text).replace(/(\r\n|\n\r|\r|\n)/g, '<br />');
         }
-    }]);
+    })
+    .filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length)) {
+                length = 10;
+            }
+            if (end === undefined) {
+                end = "...";
+            }
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length - end.length) + end;
+            }
+
+        };
+    });
