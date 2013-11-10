f6dbc11d129168593afea8e01787d1c38d843b06
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-11-10 15:32:51 -0200

    correcao layout e autenticação

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('siges', [
+        'ui.mask',
         'md5',
         'firebase',
         'siges.factories',
@@ -16,7 +17,7 @@ angular.module('siges', [
             when('/usuarios/editar/:id', {templateUrl: 'partials/usuario-detalhes.html', controller: 'UsuariosEditarCtrl'}).
             when('/usuarios', {templateUrl: 'partials/usuarios-listar.html', controller: 'UsuariosListarCtrl'}).
             when('/primeiro-acesso', {templateUrl: 'partials/primeiro-acesso.html', controller: 'PrimeiroAcessoCtrl'}).
-            when('/perfil', {templateUrl: 'partials/perfil.html', controller: 'PerfilCtrl'}).
+            when('/perfil/:id', {templateUrl: 'partials/perfil.html', controller: 'PerfilCtrl'}).
             when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'}).
             when('/404', {templateUrl: 'partials/404.html'}).
             otherwise({redirectTo: '/404'});
