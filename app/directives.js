f6e4041b7b7eaa598a8cfc0e017f690ea896e0d3
Alan Clésio da Silva Pereira <alanclesio@gmail.com>
2013-11-25 05:56:30 -0200

    Autofill correção.

--- app/directives.js
+++ app/directives.js
@@ -5,4 +5,18 @@ angular.module('siges.directives', []).
         return function (scope, elm, attrs) {
             elm.text(version);
         };
-    }]);
+    }]).
+    directive('autoFillSync', function ($timeout) {
+        return {
+            require: 'ngModel',
+            link: function (scope, elem, attrs) {
+                var origVal = elem.val();
+                $timeout(function () {
+                    var newVal = elem.val();
+                    if (ngModel.$pristine && origVal !== newVal) {
+                        ngModel.$setViewValue(newVal);
+                    }
+                }, 500);
+            }
+        }
+    });
